d15c0d019dcae3758b698511c978855cea9e55e2
Mikalai Shuhno <nickshuhno@gmail.com>
2025-01-18 20:45:39 +0100

    Move 'FeedViewController' to production target

---
+++ EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FeedViewController.swift
+//  EssentialFeed
+//
+//  Created by Mikalai Shuhno on 18/01/2025.
+//
+
+import EssentialFeed
+import UIKit
+
+final public class FeedViewController: UITableViewController  {
+    private var loader: FeedLoader?
+    
+    public init(loader: FeedLoader) {
+        self.loader = loader
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+    }
+    
+    public override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
--- EssentialFeediOSTests/FeedViewControllerTest.swift
+++ EssentialFeediOSTests/FeedViewControllerTest.swift
@@ -8,41 +8,7 @@
 import XCTest
 import UIKit
 import EssentialFeed
-
-final class FeedViewController: UITableViewController  {
-    private var loader: FeedLoader?
-    
-    init(loader: FeedLoader) {
-        self.loader = loader
-        
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-    }
-    
-    override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        
-        load()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
+import EssentialFeediOS
 
 final class FeedViewControllerTest: XCTestCase {
     func test_loadFeedActions_requestFeedFromLoader() {
