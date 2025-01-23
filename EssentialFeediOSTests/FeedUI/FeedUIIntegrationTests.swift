3641e252c8f459b5f79a95a902aa2692fc5851cb
Mikalai Shuhno <nickshuhno@gmail.com>
2025-01-23 03:38:24 +0100

    Dispatch background image feed data completion to main thread before passing result to UI components

--- EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -265,6 +265,21 @@ final class FeedUIIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadAndAppearView()
+        loader.completeFeedLoading(with: [makeImage()])
+        _ = sut.simulateFeedImageViewVisible(at: 0)
+        
+        let exp = expectation(description: "Wait for completion")
+        DispatchQueue.global().async {
+            loader.completeImageLoading(with: self.anyImageData(), at: 0)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     // MARK: Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedViewController, LoaderSpy) {
         let loader = LoaderSpy()
