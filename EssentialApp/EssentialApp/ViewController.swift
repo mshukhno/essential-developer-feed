aa5d1140f58510368b4095df75b507fe3202f8bb
Mikalai Shuhno <nickshuhno@gmail.com>
2025-01-28 00:34:46 +0100

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Mikalai Shuhno on 28/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
