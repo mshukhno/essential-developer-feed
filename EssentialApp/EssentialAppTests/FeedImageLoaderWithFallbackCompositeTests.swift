6486bbc03a10e6fbb5ffce27bac25a4917861e6a
Mikalai Shuhno <nickshuhno@gmail.com>
2025-01-29 19:00:42 +0100

    'FeedImageDataLoaderWithFallbackComposite.loadImageData' loads from primary loader first

--- EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
@@ -9,6 +9,8 @@ import XCTest
 import EssentialFeed
 
 class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
+    
     private class Task: FeedImageDataLoaderTask {
         func cancel() {
             
@@ -16,9 +18,12 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
     }
     
     init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
     }
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any EssentialFeed.FeedImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
+        
         return Task()
     }
     
@@ -36,7 +41,25 @@ class FeedImageLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
     
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
     // MARK: - Helpers
+    
+    private func anyURL() -> URL {
+        URL(string: "http://a-url.com")!
+    }
+
     private class LoaderSpy: FeedImageDataLoader {
         private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
 
