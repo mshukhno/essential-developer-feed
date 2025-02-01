b8e6c405adfe2fa43a90fb77c065909b639526d5
Mikalai Shuhno <nickshuhno@gmail.com>
2025-02-01 14:24:01 +0100

    Configures feed navigation as window root view controller

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -25,6 +25,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        configureWindow()
+    }
+    
+    func configureWindow() {
         let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         
         let remoteClient = makeRemoteClient()
@@ -35,19 +39,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
         
-        window?.rootViewController = FeedUIComposer.feedComposeWith(
-            feedLoader: FeedLoaderWithFallbackComposite(
-                primary: FeedLoaderCacheDecorator(
-                    decoratee: remoteFeedLoader,
-                    cache: localFeedLoader),
-                fallback: localFeedLoader),
-            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-                primary: localImageLoader,
-                fallback: FeedImageDataLoaderCacheDecorator(
-                    decoratee: remoteImageLoader,
-                    cache: localImageLoader
+        window?.rootViewController = UINavigationController(
+            rootViewController:
+                FeedUIComposer.feedComposeWith(
+                    feedLoader: FeedLoaderWithFallbackComposite(
+                        primary: FeedLoaderCacheDecorator(
+                            decoratee: remoteFeedLoader,
+                            cache: localFeedLoader),
+                        fallback: localFeedLoader),
+                    imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                        primary: localImageLoader,
+                        fallback: FeedImageDataLoaderCacheDecorator(
+                            decoratee: remoteImageLoader,
+                            cache: localImageLoader
+                        )
+                    )
                 )
-            )
         )
     }
     
