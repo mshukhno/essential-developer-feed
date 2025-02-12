77015584004ec476d5bd28fbe8161413a162f5f5
Mikalai Shuhno <nickshuhno@gmail.com>
2025-02-13 00:37:05 +0100

    Add default implementation for optional methods in the CellController protocol

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -14,6 +14,12 @@ public protocol CellController {
     func cancelLoad()
 }
 
+public extension CellController {
+    func preload() { }
+    
+    func cancelLoad() { }
+}
+
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView  {
     
     @IBOutlet public var errorView: ErrorView?
