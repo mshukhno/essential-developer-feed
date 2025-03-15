a1dca5dc190566cfca976a86d5a73ea328497363
Mikalai Shuhno <nickshuhno@gmail.com>
2025-03-15 14:45:56 +0100

    Duplicate FeedUIComposer as CommentsUIComposer

---
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -0,0 +1,45 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by Mikalai Shuhno on 15/03/2025.
+//
+
+import EssentialFeed
+import UIKit
+import EssentialFeediOS
+import Combine
+
+public final class CommentsUIComposer {
+    private init() { }
+    
+    public static func commentsComposeWith(
+        commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>
+    ) -> ListViewController {
+        let presentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>(loader: commentsLoader)
+        
+        let feedController = makeWith(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+        
+        presentationAdapter.presenter = LoadResourcePresenter(
+            errorView: WeakRefVirtualProxy(feedController),
+            loadingView: WeakRefVirtualProxy(feedController),
+            resourceView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher() }
+            ),
+            mapper: FeedPresenter.map
+        )
+        
+        return feedController
+    }
+    
+    private static func makeWith(title: String) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+        feedController.title = FeedPresenter.title
+        
+        return feedController
+    }
+}
---
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -0,0 +1,193 @@
+//
+//  CommentsUIIntegrationTests.swift
+//  EssentialApp
+//
+//  Created by Mikalai Shuhno on 05/03/2025.
+//
+
+import XCTest
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+import EssentialApp
+
+class CommentsUIIntegrationTests: FeedUIIntegrationTests {
+    
+    override func test_feedView_hasTitle() {
+        let (sut, _) = makeSUT()
+        sut.loadAndAppearView()
+        
+        XCTAssertEqual(sut.title, feedTitle)
+    }
+    
+    override func test_loadFeedActions_requestFeedFromLoader() {
+        let (sut, loader) = makeSUT()
+        XCTAssertEqual(loader.loadFeedCallCount, 0, "Expected no loading requests before view is loaded")
+        
+        sut.loadAndAppearView()
+        XCTAssertEqual(loader.loadFeedCallCount, 1, "Expected a loading request once view is appeared")
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(loader.loadFeedCallCount, 2, "Expected another loading request once user initiates a reload")
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(loader.loadFeedCallCount, 3, "Expected a third loading request once user initiates another reload")
+    }
+    
+    override func test_loadingFeedIndicator_isVisibleWhileLoadingFeed() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadAndAppearView()
+        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once view is loaded")
+        
+        loader.completeFeedLoading(at: 0)
+        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading completes successfully")
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once user initiates a reload")
+        
+        loader.completeFeedLoadingWithError(at: 1)
+        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading completes with error")
+    }
+    
+    override func test_loadFeedCompletion_rendersSuccessfullyLoadedFeed() {
+        let image0 = makeImage(description: "a description", location: "a location")
+        let image1 = makeImage(description: nil, location: "another location")
+        let image2 = makeImage(description: "another description", location: nil)
+        let image3 = makeImage(description: nil, location: nil)
+        let (sut, loader) = makeSUT()
+        
+        sut.loadAndAppearView()
+        assertThat(sut, isRendering: [])
+        
+        loader.completeFeedLoading(with: [image0], at: 0)
+        assertThat(sut, isRendering: [image0])
+        
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoading(with: [image0, image1, image2, image3], at: 1)
+        assertThat(sut, isRendering: [image0, image1, image2, image3])
+        runLoopToWaitUIKitFreeUpResources()
+    }
+    
+    override func test_loadFeedCompletion_rendersSuccessfullyLoadedEmptyFeedAfterNonEmptyFeed() {
+        let image0 = makeImage()
+        let image1 = makeImage()
+        let (sut, loader) = makeSUT()
+        
+        sut.loadAndAppearView()
+        loader.completeFeedLoading(with: [image0, image1], at: 0)
+        assertThat(sut, isRendering: [image0, image1])
+        
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoading(with: [], at: 1)
+        assertThat(sut, isRendering: [])
+        runLoopToWaitUIKitFreeUpResources()
+    }
+    
+    override func test_loadFeedCompletion_doesNotAlterCurrentRenderingStateOnError() {
+        let image0 = makeImage()
+        let (sut, loader) = makeSUT()
+        
+        sut.loadAndAppearView()
+        loader.completeFeedLoading(with: [image0], at: 0)
+        assertThat(sut, isRendering: [image0])
+        
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoadingWithError(at: 1)
+        assertThat(sut, isRendering: [image0])
+        runLoopToWaitUIKitFreeUpResources()
+    }
+    
+    override func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadAndAppearView()
+        
+        let exp = expectation(description: "Wait for completion")
+        DispatchQueue.global().async {
+            loader.completeFeedLoading(at: 0)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    override func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadAndAppearView()
+        XCTAssertEqual(sut.errorMessage, nil)
+        
+        loader.completeFeedLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, loadError)
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
+    
+    override func test_tapOnErrorView_hidesErrorMessage() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadAndAppearView()
+        XCTAssertEqual(sut.errorMessage, nil)
+        
+        loader.completeFeedLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, loadError)
+        
+        sut.simulateErrorViewTap()
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
+    
+    // MARK: Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (ListViewController, LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = CommentsUIComposer.commentsComposeWith(commentsLoader: loader.loadPublisher)
+        sut.replaceRefreshControlWithFakeForiOS17Support()
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, loader)
+    }
+    
+    private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "https://any-url.com")!) -> FeedImage {
+        FeedImage(id: UUID(), description: description, location: location, url: url)
+    }
+    
+    private func runLoopToWaitUIKitFreeUpResources() {
+        RunLoop.current.run(until: Date()+1)
+    }
+    
+}
+
+private extension ListViewController {
+    
+    func replaceRefreshControlWithFakeForiOS17Support() {
+        let fake = FakeRefreshControl()
+        
+        refreshControl?.allTargets.forEach { target in
+            refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                fake.addTarget(target, action: Selector(action), for: .valueChanged)
+            }
+        }
+        
+        refreshControl = fake
+    }
+    
+}
+
+private class FakeRefreshControl: UIRefreshControl {
+    
+    private var _isRefreshing: Bool = false
+    
+    override var isRefreshing: Bool {
+        _isRefreshing
+    }
+    
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    
+    override func endRefreshing() {
+        _isRefreshing = false
+    }
+    
+}
