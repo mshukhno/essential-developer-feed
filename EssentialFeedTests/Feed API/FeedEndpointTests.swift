127bcfab1b7b223cd0d1a56553ec276f3a31b425
Mikalai Shuhno <nickshuhno@gmail.com>
2025-04-04 16:13:58 +0200

    Implement FeedEndpoint

---
+++ EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -0,0 +1,24 @@
+//
+//  FeedEndpointTests.swift
+//  EssentialFeed
+//
+//  Created by Mikalai Shuhno on 04/04/2025.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedEndpointTests: XCTestCase {
+
+    func test_feed_endpointURL() {
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = FeedEndpoint.get.url(baseURL: baseURL)
+        
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url.com", "host")
+        XCTAssertEqual(received.path, "/v1/feed", "path")
+        XCTAssertEqual(received.query, "limit=10", "query")
+    }
+
+}
