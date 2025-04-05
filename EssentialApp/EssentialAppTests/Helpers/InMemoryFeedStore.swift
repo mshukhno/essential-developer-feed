31852f615ffc9c50ceedf42aaa588982d069dfad
Mikalai Shuhno <nickshuhno@gmail.com>
2025-04-05 15:32:47 +0200

    Make in memory FeedStore implementation sync

--- EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
+++ EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -5,31 +5,33 @@
 //  Created by Mikalai Shuhno on 01/02/2025.
 //
 
-import EssentialFeed
 import Foundation
+import EssentialFeed
 
-class InMemoryFeedStore: FeedStore, FeedImageDataStore {
+class InMemoryFeedStore {
     private(set) var feedCache: CachedFeed?
     private var feedImageDataCache: [URL: Data] = [:]
     
     private init(feedCache: CachedFeed? = nil) {
         self.feedCache = feedCache
     }
-    
-    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+}
+
+extension InMemoryFeedStore: FeedStore {
+    func deleteCachedFeed() throws {
         feedCache = nil
-        completion(.success(()))
     }
-    
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
         feedCache = CachedFeed(feed: feed, timestamp: timestamp)
-        completion(.success(()))
     }
-    
-    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.success(feedCache))
+
+    func retrieve() throws -> CachedFeed? {
+        feedCache
     }
-    
+}
+
+extension InMemoryFeedStore: FeedImageDataStore {
     func insert(_ data: Data, for url: URL) throws {
         feedImageDataCache[url] = data
     }
